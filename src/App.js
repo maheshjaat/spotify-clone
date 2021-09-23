af0f51520abe880d0b91bf10d68b63fd596e6d6e
maheshjaat <pra.mj99@gmail.com>
2021-09-23 12:52:22 +0530

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import './App.css';
 import Login from './Login';
 import {getTokenFromResponse} from "./spotify"
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify =new SpotifyWebApi()
 
 function App() {
   
-
+  const[token,setToken]=useState(null)
 
 //run code based on given condition
 useEffect(() => {
   const hash =getTokenFromResponse()
    window.location.hash ="";
 
-   const token=hash.access_token;
+   const _token=hash.access_token;
+   if(_token){
+     setToken(_token)
+
+     spotify.setAccessToken(_token)
+
+     spotify.getMe().then(user => {
+       console.log("person", user);
+     })
+   }
 
   console.log('I HAVE A TOKEN >>>',token);
 },[])
 
   return (
     <div className="app">
+      { token ? <Player/> : (<Login />)}
+          
+          
+          
+        
       
 
       {/* spotify logo */}
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div>
+            <h1>welcome to sp</h1> 
+        </div>
+    )
+}
+
+export default Player
